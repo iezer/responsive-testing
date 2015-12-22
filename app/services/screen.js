be7d0d11dc1a6723016db666b3a3213e4501218b
Isaac Ezer <isaac.ezer@gmail.com>
2015-12-22 11:59:40 -0500

    refactor - reuse minDesktopWidth variable

--- app/services/screen.js
+++ app/services/screen.js
@@ -1,8 +1,8 @@
 import EmberScreen, { breakpoint } from 'ember-screen';
 
-export const minDesktopWidth = '480px';
+export const minDesktopWidth = 480;
 
 export default EmberScreen.extend({
-  isMobile: breakpoint('(max-width: 479px)'),
-  isDesktop: breakpoint(`(min-width: ${minDesktopWidth})`)
+  isMobile: breakpoint(`(max-width: ${minDesktopWidth - 1}px)`),
+  isDesktop: breakpoint(`(min-width: ${minDesktopWidth}px)`)
 });
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -8,7 +8,7 @@ import { minDesktopWidth } from '../services/screen';
 QUnit.begin(() => {
   let mqVal = window.Modernizr.mq(`(min-width: ${minDesktopWidth})`);
   if (!mqVal) {
-    console.warn(`Acceptance tests require a minimum screen width of ${minDesktopWidth}. Please expand the window and/or reduce the size of the dev tools.`);
+    console.warn(`Acceptance tests require a minimum screen width of ${minDesktopWidth}px. Please expand the window and/or reduce the size of the dev tools.`);
   }
 });
 
