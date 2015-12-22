545e05b8aac7a72e2d49f5446fa970e95d0a68ac
Isaac Ezer <isaac.ezer@gmail.com>
2015-12-22 09:55:13 -0500

    use modernizr to warn on screen size

--- app/services/screen.js
+++ app/services/screen.js
@@ -1,6 +1,8 @@
 import EmberScreen, { breakpoint } from 'ember-screen';
 
+export const minDesktopWidth = '480px';
+
 export default EmberScreen.extend({
   isMobile: breakpoint('(max-width: 479px)'),
-  isDesktop: breakpoint('(min-width: 480px)')
+  isDesktop: breakpoint(`(min-width: ${minDesktopWidth})`)
 });
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,15 @@
+/* global QUnit */
 import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
+import { minDesktopWidth } from '../services/screen';
+
+QUnit.begin(() => {
+  let mqVal = window.Modernizr.mq(`(min-width: ${minDesktopWidth})`);
+  if (!mqVal) {
+    console.warn(`Acceptance tests require a minimum screen width of ${minDesktopWidth}. Please expand the window and/or reduce the size of the dev tools.`);
+  }
+});
 
 setResolver(resolver);
