92a910e2f91a54f1b33e2e44502f18fc95d8d888
Isaac Ezer <isaac.ezer@gmail.com>
2015-12-22 09:27:40 -0500

    responsive tests
     - work by stubbing service regardless of browser size

--- tests/integration/pods/components/nav-bar/component-test.js
+++ tests/integration/pods/components/nav-bar/component-test.js
@@ -1,16 +1,36 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
-moduleForComponent('nav-bar', 'Integration | Component | nav bar', {
-  integration: true
+const screenServiceStub = Ember.Service.extend({
+  isMobile: false,
+  isDesktop: true
 });
 
-test('it renders', function(assert) {
+moduleForComponent('nav-bar', 'Integration | Component | nav bar', {
+  integration: true,
 
+  beforeEach() {
+    this.register('service:screen', screenServiceStub);
+    this.inject.service('screen');
+  }
+});
+
+test('it renders in desktop mode', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
 
   this.render(hbs`{{nav-bar}}`);
 
   assert.equal(this.$().text().trim(), 'Desktop Nav Bar');
+});
+
+test('it renders in mobile mode', function(assert) {
+  this.set('screen.isMobile', true);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  this.render(hbs`{{nav-bar}}`);
+
+  assert.equal(this.$().text().trim(), '☰');
 });
